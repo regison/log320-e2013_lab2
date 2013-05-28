3b7e3c1c03eabca73984800f02e9a8edffbf07da
Alex Kua <alexkua@me.com>
2013-05-27 21:47:49 -0400

    Nous avons fait la classe SudokuMoveValidator qui contient la méthode estValide (2e objectif)

--- LOG320_LAB2/src/solver/SudokuMoveValidator.java
+++ LOG320_LAB2/src/solver/SudokuMoveValidator.java
@@ -1,5 +1,56 @@
 package solver;
+import java.math.*;
 
 public class SudokuMoveValidator {
 
+	private final int MAX_LENGTH = 9;
+	private String sudokuGril[][];
+	
+	public SudokuMoveValidator(String[][] sudokuGril)
+	{
+		this.sudokuGril = sudokuGril;
+	}
+	
+	public boolean estValide(int x, int y, String k)
+	{
+		boolean isValid = true;
+		while(isValid)
+		{
+			// verifies line
+			for(int i = 0; i < MAX_LENGTH; i++)
+			{
+				if(k == sudokuGril[x][i])
+				{
+					isValid = false;
+				}
+			}
+			
+			// verifies column
+			for(int i = 0; i < MAX_LENGTH; i++)
+			{
+				if(k == sudokuGril[i][y])
+				{
+					isValid = false;
+				}
+			}
+			
+			// verifies 3x3 block
+			int rowStart = (int)Math.floor(x/3) * 3;
+			int rowLength = ((int)Math.floor(x/3) * 3) + 3;
+			int columnStart = (int)Math.floor(y/3) * 3;
+			int columnLength = ((int)Math.floor(y/3) * 3) + 3;
+			
+			for(int i = rowStart; i < rowLength; i++)
+			{
+				for(int j = columnStart; j < columnLength; j++)
+				{
+					if(k == sudokuGril[i][j])
+					{
+						isValid = false;
+					}
+				}
+			}
+		}
+		return isValid;
+	}
 }
