1a3b80bd2f2ef12e8021f47b5e465a8fc9fdb043
regison <reginald.augustin@gmail.com>
2013-05-27 23:06:59 -0700

    [RAU] Commit file reader test class

--- LOG320_LAB2/src/tests/SudokuFileReaderTest.java
+++ LOG320_LAB2/src/tests/SudokuFileReaderTest.java
@@ -1,5 +1,18 @@
 package tests;
 
-public class SudokuFileReaderTest {
+import junit.framework.TestCase;
+import lecture.SudokuFileReader;
 
+public class SudokuFileReaderTest extends TestCase {
+
+	String file ="";
+	public SudokuFileReaderTest(){
+		file = "./filesTest/sudoku2.txt";
+	}
+	
+	public void testRead(){
+		SudokuFileReader reader = new SudokuFileReader(file);
+		
+		reader.printSudokuGril();
+	}
 }
